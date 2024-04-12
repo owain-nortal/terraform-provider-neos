2efdd73dceb1fbdbf662edae2e8be858bfd4c226
owain perry <owain.perry@nortal.com>
2024-04-12 11:13:02 +0100

    fixup linting

--- internal/provider/registry_core_data_source_test.go
+++ internal/provider/registry_core_data_source_test.go
@@ -1,34 +1,34 @@
 package provider
 
 import (
-	//"testing"
+	"testing"
 
-	//"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
-// func TestAccRegistryCoreDataSource(t *testing.T) {
-// 	resource.Test(t, resource.TestCase{
-// 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
-// 		Steps: []resource.TestStep{
-// 			// Read testing
-// 			{
-// 				Config: providerConfig + `data "neos_registry_core" "test" {}`,
-// 				Check: resource.ComposeAggregateTestCheckFunc(
-// 					// Verify number of coffees returned
-// 					resource.TestCheckResourceAttr("data.neos_registry_core.test", "registry_cores.#", "9"),
-// 					// Verify the first coffee to ensure all attributes are set
-// 					//resource.TestCheckResourceAttr("data.neos_registry_core.test", "registry_cores.0.description", ""),
-// 					//resource.TestCheckResourceAttr("data.neos_registry_core.test", "registry_cores.0.id", "1"),
-// 					// resource.TestCheckResourceAttr("data.neos_registry_core.test", "registry_cores.0.image", "/hashicorp.png"),
-// 					// resource.TestCheckResourceAttr("data.neos_registry_core.test", "coffees.0.ingredients.#", "1"),
-// 					// resource.TestCheckResourceAttr("data.neos_registry_core.test", "coffees.0.ingredients.0.id", "6"),
-// 					// resource.TestCheckResourceAttr("data.neos_registry_core.test", "coffees.0.name", "HCP Aeropress"),
-// 					// resource.TestCheckResourceAttr("data.neos_registry_core.test", "coffees.0.price", "200"),
-// 					// resource.TestCheckResourceAttr("data.neos_registry_core.test", "coffees.0.teaser", "Automation in a cup"),
-// 					// Verify placeholder id attribute
-// 					resource.TestCheckResourceAttr("data.neos_registry_core.test", "registry_cores.0.urn", "00000000-0000-0000-0000-000000000001"),
-// 				),
-// 			},
-// 		},
-// 	})
-// }
+func TestAccRegistryCoreDataSource(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Read testing
+			{
+				Config: providerConfig + `data "neos_registry_core" "test" {}`,
+				Check:  resource.ComposeAggregateTestCheckFunc(
+				// Verify number of coffees returned
+				//resource.TestCheckResourceAttr("data.neos_registry_core.test", "registry_cores.#", "9"),
+				// Verify the first coffee to ensure all attributes are set
+				//resource.TestCheckResourceAttr("data.neos_registry_core.test", "registry_cores.0.description", ""),
+				//resource.TestCheckResourceAttr("data.neos_registry_core.test", "registry_cores.0.id", "1"),
+				// resource.TestCheckResourceAttr("data.neos_registry_core.test", "registry_cores.0.image", "/hashicorp.png"),
+				// resource.TestCheckResourceAttr("data.neos_registry_core.test", "coffees.0.ingredients.#", "1"),
+				// resource.TestCheckResourceAttr("data.neos_registry_core.test", "coffees.0.ingredients.0.id", "6"),
+				// resource.TestCheckResourceAttr("data.neos_registry_core.test", "coffees.0.name", "HCP Aeropress"),
+				// resource.TestCheckResourceAttr("data.neos_registry_core.test", "coffees.0.price", "200"),
+				// resource.TestCheckResourceAttr("data.neos_registry_core.test", "coffees.0.teaser", "Automation in a cup"),
+				// Verify placeholder id attribute
+				//resource.TestCheckResourceAttr("data.neos_registry_core.test", "registry_cores.0.urn", "00000000-0000-0000-0000-000000000001"),
+				),
+			},
+		},
+	})
+}
